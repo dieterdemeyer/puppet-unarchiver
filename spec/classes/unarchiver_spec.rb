206e711f49da773cbcd668dac33ac83b3181286a
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-23 16:15:29 +0100

    Added spec tests

---
+++ spec/classes/unarchiver_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'unarchiver' do
+
+  it { should contain_class('unarchiver') }
+  it { should contain_package('Unarchiver').with_provider('compressed_app') }
+  it { should contain_package('Unarchiver').with_source('http://theunarchiver.googlecode.com/files/TheUnarchiver3.6.1.zip') }
+
+end
