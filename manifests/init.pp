eb5c17a8f3d99bbfc4a0b4a64dcd511d462e618d
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-11-19 13:58:49 +0100

    Added version to package resource name

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,10 @@
 #
 #  include unarchiver
 class unarchiver {
-  package { 'Unarchiver':
+
+  package { 'Unarchiver-3.9.1':
     provider => 'compressed_app',
     source   => 'https://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip',
   }
+
 }
--- spec/classes/unarchiver_spec.rb
+++ spec/classes/unarchiver_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'unarchiver' do
 
   it { should contain_class('unarchiver') }
-  it { should contain_package('Unarchiver').with_provider('compressed_app') }
-  it { should contain_package('Unarchiver').with_source('https://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip') }
+  it { should contain_package('Unarchiver-3.9.1').with_provider('compressed_app') }
+  it { should contain_package('Unarchiver-3.9.1').with_source('https://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip') }
 
 end
